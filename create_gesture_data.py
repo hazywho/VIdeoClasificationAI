d12ceb2e0dc78cd341c80a0f884dbd35ad6d2994
hazywho <116442947+hazywho@users.noreply.github.com>
2024-03-18 22:31:23 +0800

    s

--- create_gesture_data.py
+++ create_gesture_data.py
@@ -104,9 +104,9 @@ while True:
             
             # Displaying the thresholded image
             cv2.imshow("Thresholded Hand Image", thresholded)
-            if num_imgs_taken <= 300:
+            if num_imgs_taken <= 600:
                 #cv2.imwrite(r"D:\\gesture\\train\\"+str(element)+"\\" + str(num_imgs_taken+300) + '.jpg', thresholded)
-                cv2.imwrite(r"C:\Users\zanyi\OneDrive\Git hub\VIdeoClasificationAI\Tes\03" + "/" + str(num_imgs_taken) + '.jpg', thresholded)
+                cv2.imwrite(r"C:\Users\zanyi\OneDrive\Git hub\VIdeoClasificationAI\Train\02" + "/" + str(num_imgs_taken) + '.jpg', thresholded)
             else:
                 break
             num_imgs_taken +=1
